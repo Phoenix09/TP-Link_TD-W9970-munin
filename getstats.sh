8e0726fd100c4cf60f1de64b0a23a28afb02f25a
Phoenix09 <Phoenix09@users.noreply.github.com>
2016-08-25 16:11:41 +0100

    getstats.sh: tee to output file

--- getstats.sh
+++ getstats.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 statsfile=${statsfile:-/var/lib/munin/plugin-state/TP-Link_TD-W9970-munin-output.txt}
-expect `dirname $0`/TD-W9970.expect  | tee | tr -d "\015"  > $statsfile
+expect `dirname $0`/TD-W9970.expect  | tee | tr -d "\015"  | tee $statsfile
