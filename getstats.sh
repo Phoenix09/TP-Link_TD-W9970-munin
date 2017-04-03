f471c1264c59c03cbf11f7cac03c01abfc5cdfb6
Phoenix09 <Phoenix09@users.noreply.github.com>
2017-04-03 17:45:34 +0100

    getstats.sh: Use temp file in case stats fetching fails

--- getstats.sh
+++ getstats.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-statsfile=${statsfile:-/var/lib/munin/plugin-state/TP-Link_TD-W9970-munin-output.txt}
-expect `dirname $0`/TD-W9970.expect  | tee | tr -d "\015"  | tee $statsfile
+set -e
+statsfile="${statsfile:-/var/lib/munin/plugin-state/TP-Link_TD-W9970-munin-output.txt}"
+expect "$(dirname "$(readlink -f "$0")")/TD-W9970.expect" | tr -d "\015"  | tee "$statsfile.tmp"
+if [ -s "$statsfile.tmp" ]; then
+        mv "$statsfile.tmp" "$statsfile"
+else
+        echo "Empty stats file" >&2
+fi
+
